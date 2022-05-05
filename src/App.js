d0e83c88b02781103ddc2380a30879df5e643ec4
jwill617boston <jwilliams617bos@gmial.com>
2022-05-05 19:05:57 -0400

    navbar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,68 @@
 import "./App.css";
-import Profile from "./comp/profile/Profile";
+
 
 import styled from "styled-components";
 
-const StyledComp = styled.div``;
+const Container = styled.div`
+   height: 50px;
+`;
+
+const Wrapper = styled.div`
+   padding: 10px 20px;
+   display: flex;
+   align-items: center;
+   justify-content: space-between;
+`;
+
+const Left = styled.div`
+   width: 60%;
+   display: flex;
+   align-items: center;
+   justify-content: space-between;
+`;
+
+const Logo = styled.h1`
+   font-weight: bold;
+   text-decoration: underline crimson;
+`;
+
+const Menu = styled.ul`
+   display: flex;
+   list-style: none;
+`;
+
+const MenuItem = styled.li`
+   margin-right: 30px;
+   font-size: 20px;
+   font-weight: bold;
+   color: grey;
+`;
+
+const Button = styled.button`
+   border: 2px solid black;
+   padding: 10px 15px;
+   background-color: crimson;
+   color: white;
+   font-weight: bold;
+`;
+
 function App() {
    return (
-      <div className="App">
-         <StyledComp>
-            <header className="App-header">
-               <p>Jason is that dude</p>
-               <Profile />
-            </header>
-         </StyledComp>
-      </div>
+      <Container>
+         <Wrapper>
+            <Left>
+               <Logo>Agency</Logo>
+               <Menu>
+                  <MenuItem>Home</MenuItem>
+                  <MenuItem>Features</MenuItem>
+                  <MenuItem>Services</MenuItem>
+                  <MenuItem>Pricing</MenuItem>
+                  <MenuItem>Contact</MenuItem>
+               </Menu>
+            </Left>
+            <Button>JOIN NOW</Button>
+         </Wrapper>
+      </Container>
    );
 }
 
