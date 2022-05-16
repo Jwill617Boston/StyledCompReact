3c9330846ac16e9009a0aa3578c9215985816d79
jwill617boston <jwilliams617bos@gmial.com>
2022-05-16 17:56:04 -0400

    Contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import styled from "styled-components";
+import Map from "../img/map.png";
+import Phone from "../img/phone.png";
+import Send from "../img/send.png";
 
 const Container = styled.div`
    height: 90%;
@@ -18,6 +21,11 @@ const FormContainer = styled.div`
    width: 50%;
 `;
 
+const Title = styled.h1`
+   margin: 50px;
+   margin-top: 0;
+`;
+
 const Form = styled.form`
    height: 250px;
    display: flex;
@@ -25,9 +33,42 @@ const Form = styled.form`
    justify-content: center;
 `;
 
-const LeftForm = styled.div``;
+const LeftForm = styled.div`
+   height: 100%;
+   display: flex;
+   flex-direction: column;
+   justify-content: space-between;
+   margin-right: 20px;
+`;
 
-const RightForm = styled.div``;
+const RightForm = styled.div`
+   height: 100%;
+   display: flex;
+   flex-direction: column;
+   justify-content: space-between;
+`;
+
+const Input = styled.input`
+   width: 200px;
+   padding: 20px;
+`;
+
+const Button = styled.button`
+   border: none;
+   padding: 15px;
+   background-color: darkblue;
+   color: white;
+   font-size: 20px;
+   border-radius: 10px;
+   margin-top: 20px;
+   cursor: pointer;
+`;
+
+const TextArea = styled.input`
+   width: 200px;
+   height: 60%;
+   padding: 20px;
+`;
 
 const AddressContainer = styled.div`
    width: 50%;
@@ -36,14 +77,56 @@ const AddressContainer = styled.div`
    align-items: center;
 `;
 
+const AddressItem = styled.div`
+   display: flex;
+   align-items: center;
+   margin-bottom: 50px;
+`;
+
+const Icon = styled.img`
+   width: 20px;
+   margin-right: 20px;
+`;
+const Text = styled.span`
+   font-size: 20px;
+`;
+
 const Contact = () => {
    return (
       <Container>
          <Wrapper>
             <FormContainer>
-               <Form>F</Form>
+               <Title>
+                  Questions? <br /> Let's Get In Touch
+               </Title>
+               <Form>
+                  <LeftForm>
+                     <Input placeholder="Your Name" />
+                     <Input placeholder="Your Email" />
+                     <Input placeholder="Subject" />
+                  </LeftForm>
+                  <RightForm>
+                     <TextArea placeholder="Your Message" />
+                     <Button>Send</Button>
+                  </RightForm>
+               </Form>
             </FormContainer>
-            <AddressContainer>A</AddressContainer>
+            <AddressContainer>
+               <AddressItem>
+                  <Icon src={Map} />
+                  <Text>123 Park Avenue St., New York, USA</Text>
+               </AddressItem>
+               <AddressItem>
+                  <Icon src={Phone} />
+                  <Text>+1 631 1234 5678</Text>
+                  <Text>+1 326 1234 5678</Text>
+               </AddressItem>
+               <AddressItem>
+                  <Icon src={Send} />
+                  <Text>contact@lama.dev</Text>
+                  <Text>sales@lama.dev</Text>
+               </AddressItem>
+            </AddressContainer>
          </Wrapper>
       </Container>
    );
