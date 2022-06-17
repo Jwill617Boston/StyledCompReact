1a77cca36c1569622504b713b270d27f7af06261
Jwill617Boston <77422031+Jwill617Boston@users.noreply.github.com>
2022-06-17 08:01:19 -0400

    Locked Links

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Register from "./components/regester/Regester";
 import UserPage from "./components/userpage/UserPage";
 import Layout from "./components/layout/Layout";
 import ErrorPage from "./components/errorpage/ErrorPage";
+import RequireAuth from "./components/layout/RequiredAuth";
 
 const ROLES = {
    User: 2001,
@@ -21,9 +22,12 @@ const App = () => {
                <Route path="home" element={<Home />} />
                <Route path="login" element={<Login />} />
                <Route path="register" element={<Register />} />
-               {/* user routes */}
-               <Route path="userpage" element={<UserPage />} />
+               <Route element={<RequireAuth />}>
+                  {/* user routes */}
+                  <Route path="userpage" element={<UserPage />} />
+               </Route>
             </Route>
+
             <Route path="*" element={<ErrorPage />} />
          </Routes>
       </>
---
+++ src/components/layout/RequiredAuth.jsx
@@ -0,0 +1,15 @@
+import { useLocation, Navigate, Outlet } from "react-router-dom";
+import useAuth from "../../utilities/useAuth";
+
+const RequireAuth = ({ allowedRoles }) => {
+   const { auth } = useAuth();
+   const location = useLocation();
+
+   return auth?.user ? (
+      <Outlet />
+   ) : (
+      <Navigate to="home" state={{ from: location }} replace />
+   );
+};
+
+export default RequireAuth;
