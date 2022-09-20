d7349f4d432c4072827e24ec9bfe34ed253d91b8
Jwill617Boston <77422031+Jwill617Boston@users.noreply.github.com>
2022-09-20 19:53:49 -0400

    Total Restart

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from "react-dom/client";
 import App from './App';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./utilities/AuthProvider";
-import { PersistGate } from "redux-persist/integration/react";
 import { Provider } from "react-redux";
-import { store, persistor } from "./redux/store";
+import { store } from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,11 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { persistStore } from "redux-persist";
-import userSlice from "../redux/userSlice";
+
 
 export const store = configureStore({
-   reducer: {
-      user: userSlice,
-   },
+   reducer: {},
 });
 
-export const persistor = persistStore(store);
