c42bf0aa56c13b2677ba032d662cbd72e26855bc
Jwill617Boston <77422031+Jwill617Boston@users.noreply.github.com>
2023-03-22 22:01:29 -0400

    new

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import "./app.css";
+import { useState, useEffect } from "react";
 import CompOne from "./components/CompOne";
+import axios from "axios";
 
 const App = () => {
+   const [data, setData] = useState(["hello"]);
+   useEffect(() => {
+      axios.get("https://api.publicapis.org/entries").then((res) => {
+         setData(res.data.entries);
+      });
+   }, []);
+
+   console.log(data);
+
    return (
       <>
-         <div className="content1">
-            <CompOne />
-         </div>
          <section>
-            <h1>Hello World</h1>
+            <button>Get Data</button>
+            <h1>Data</h1>
+            <ul></ul>
          </section>
       </>
    );
