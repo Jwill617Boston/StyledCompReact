d19d7f4295018acf56a5c09c70c4deb933189e43
Jwill617Boston <77422031+Jwill617Boston@users.noreply.github.com>
2023-04-01 11:23:32 -0400

    Router 2

--- src/components/NavbarComp.jsx
+++ src/components/NavbarComp.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const NavbarComp = () => {
-   return <nav>NavbarComp</nav>;
+   return (
+      <nav>
+         <ul>
+            <li>
+               <Link to={`/contacts/1`}>Your Name</Link>
+            </li>
+            <li>
+               <Link to={`/contacts/2`}>Your Friend</Link>
+            </li>
+         </ul>
+      </nav>
+   );
 };
 
 export default NavbarComp;
--- src/index.js
+++ src/index.js
@@ -2,20 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import ErrorPage from "./pages/ErrorPage";
-import Root from "./routes/root";
-import Contact from "./routes/contact";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import "./index.css";
 
 const router = createBrowserRouter([
    {
       path: "/",
-      element: <Root />,
+      element: <App />,
       errorElement: <ErrorPage />,
-   },
-   {
-      path: "contacts/:contactId",
-      element: <Contact />,
    },
 ]);
 
