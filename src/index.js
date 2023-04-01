d531e3734d4e555212ee03efe29bdd5d99d44285
Jwill617Boston <77422031+Jwill617Boston@users.noreply.github.com>
2023-04-01 11:08:59 -0400

    Router new

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import ErrorPage from "./pages/ErrorPage";
+import Root from "./routes/root";
+import Contact from "./routes/contact";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
+
+const router = createBrowserRouter([
+   {
+      path: "/",
+      element: <Root />,
+      errorElement: <ErrorPage />,
+   },
+   {
+      path: "contacts/:contactId",
+      element: <Contact />,
+   },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
    <React.StrictMode>
-      <BrowserRouter>
-         <App />
-      </BrowserRouter>
+      <RouterProvider router={router} />
    </React.StrictMode>
 );
